6ad9ef52b36b54e27f354b9689a45da21d4b2c72
Darwin Algarin <dalgarins@live.com>
2020-05-10 17:27:08 -0500

    Added test for gps.

--- src/test/java/co/s4n/corrientazos/domain/drone/GPSTest.java
+++ src/test/java/co/s4n/corrientazos/domain/drone/GPSTest.java
@@ -1,7 +1,22 @@
 package co.s4n.corrientazos.domain.drone;
 
+import co.s4n.corrientazos.domain.location.Location;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class GPSTest {
 
+    @Test
+    public void givenInitialLocationAndMove4StepsToNorth() {
+        Location initial = new Location(0,0);
+        GPS gps = new GPS(initial);
+        gps.moveToNorth();
+        gps.moveToNorth();
+        gps.moveToNorth();
+        gps.moveToNorth();
+
+        Location expected = new Location(0, 4);
+        assertEquals(expected, gps.getCurrentLocation());
+    }
 }
