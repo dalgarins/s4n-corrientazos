f8ed874b60d904e9372e9e85d13f1442847d415b
Darwin Algarin <dalgarins@live.com>
2020-05-10 17:34:12 -0500

    Added save test report.

--- src/test/java/co/s4n/corrientazos/data/repository/RoutesRepositoryTest.java
+++ src/test/java/co/s4n/corrientazos/data/repository/RoutesRepositoryTest.java
@@ -1,17 +1,25 @@
 package co.s4n.corrientazos.data.repository;
 
+import co.s4n.corrientazos.domain.location.Location;
+import co.s4n.corrientazos.domain.orientation.Cardinal;
+import co.s4n.corrientazos.domain.report.DroneReport;
+import co.s4n.corrientazos.domain.report.IDroneReport;
 import co.s4n.corrientazos.domain.report.IWriteReport;
 import co.s4n.corrientazos.domain.repository.IInputRepository;
+import co.s4n.corrientazos.domain.repository.IOutputRepository;
 import co.s4n.corrientazos.domain.route.DroneRoute;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RoutesRepositoryTest {
@@ -34,4 +42,19 @@ public class RoutesRepositoryTest {
         assertFalse(droneRoutes.isEmpty());
     }
 
+    @Test
+    public void givenListDroneReportsCreateOutputReports() {
+        IWriteReport writeReport = Mockito.mock(IWriteReport.class);
+        IOutputRepository outputRepository = new RoutesRepository("input_test/", writeReport);
+
+        List<IDroneReport> reports = getReports();
+
+        outputRepository.saveDroneReports(reports);
+        verify(writeReport).writeReport("out01.txt", reports);
+    }
+
+    private List<IDroneReport> getReports() {
+        IDroneReport report = new DroneReport("in01.txt", new Location(4,2), Cardinal.EAST);
+        return Arrays.asList(report);
+    }
 }
